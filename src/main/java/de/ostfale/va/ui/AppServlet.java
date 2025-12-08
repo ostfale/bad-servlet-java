c8b22ce5dce81fdfbe5a91f7bd1f88cb9fac9047
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2025-12-08 09:00:59 +0100

    Remove custom error handling in AppServlet and simplify configuration
    - Replaced custom implementation of `AppServlet` with a basic extension of `VaadinServlet`.
    - Moved servlet configuration from annotations to `web.xml` for clarity.
    - Updated application version to `1.0.0`.

--- src/main/java/de/ostfale/va/ui/AppServlet.java
+++ src/main/java/de/ostfale/va/ui/AppServlet.java
@@ -1,34 +1,6 @@
 package de.ostfale.va.ui;
 
-import com.vaadin.flow.server.ErrorHandler;
 import com.vaadin.flow.server.VaadinServlet;
-import com.vaadin.flow.server.VaadinServletService;
-import de.ostfale.va.common.UseLogging;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 
-
-@WebServlet(urlPatterns = "/*", name = "Custom AppServlet", asyncSupported = true, loadOnStartup = 1)
-public class AppServlet extends VaadinServlet implements UseLogging {
-
-    @Override
-    protected void servletInitialized() throws ServletException {
-        super.servletInitialized();
-        log().info("AppServlet :: servletInitialized");
-        configureSessionInitListener();
-    }
-
-    private void configureSessionInitListener() {
-        VaadinServletService service = getService();
-        service.addSessionInitListener(event ->
-                event.getSession().setErrorHandler(createSessionErrorHandler())
-        );
-    }
-
-    private ErrorHandler createSessionErrorHandler() {
-        return errorEvent -> {
-            Throwable throwable = errorEvent.getThrowable();
-            log().error("Session error occurred", throwable);
-        };
-    }
+public class AppServlet extends VaadinServlet {
 }
